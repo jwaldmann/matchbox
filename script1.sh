5c8b5f96388921100ba17f340e990f0d25dd1bf0
noeth <noeth@57298710-c1cb-11e0-96fc-5fe904bb4466>
2013-01-16 15:45:24 +0000

    some corrections in example
    git-svn-id: svn+ssh://algire3.informatik.uni-leipzig.de/local/svn/NewRepair@507 57298710-c1cb-11e0-96fc-5fe904bb4466

--- script1.sh
+++ script1.sh
@@ -4,12 +4,13 @@ pathMB="/home/eric/NewRepair/trunk/TRS/code/dist/build/mb/mb"
 
 #pathFiles="/home/eric/NewRepair/trunk/TRS/files/TRS_Standard/ -name '*.xml'"
 
+pathFiles="/home/eric/NewRepair/trunk/TRS/files/TRS_Standard/"
+
 logfile=all.log
 
-rm -f $logfile
 touch $logfile
 
-find /home/eric/NewRepair/trunk/TRS/files/TRS_Standard/ -name '*.xml' | while read file;
+find $pathFiles -name '*.xml' | while read file;
 do
 	echo $file >> $logfile
 	timeout 180 $pathMB $file  >> $logfile 2>&1
