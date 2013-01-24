20a9ec147dec8beec3c5c209c62805f4d4abc7f3
noeth <noeth@57298710-c1cb-11e0-96fc-5fe904bb4466>
2013-01-24 20:45:17 +0000

    script update
    git-svn-id: svn+ssh://algire3.informatik.uni-leipzig.de/local/svn/NewRepair@527 57298710-c1cb-11e0-96fc-5fe904bb4466

--- script1.sh
+++ script1.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 pathMB="/home/eric/NewRepair/trunk/TRS/code/dist/build/mb/mb"
-#pathFiles="/home/eric/NewRepair/trunk/TRS/files/SRS_Relative/ICFP_2010_relative"
+pathFiles="/home/eric/NewRepair/trunk/TRS/files/SRS_Relative/ICFP_2010_relative"
 
-pathFiles="data/TRS_Standard/Transformed_CSR_04"
+#pathFiles="data/TRS_Standard/Transformed_CSR_04"
 
-logfile_nocompress=all_no_ICFP_2010_relative.log
+logfile_nocompress=testing_no.log
 logfile_simplecompress=all_ICFP_2010_relative.log
-logfile_compress=all_compress_ICFP_2010_relative.log
-logfile_done=all_done.log
+logfile_compress=testing_compress.log
+logfile_done=log
+
+options_for_all="--bits=4 -s"
 
 touch $logfile_nocompress
 touch $logfile_simplecompress
@@ -18,14 +20,14 @@ find $pathFiles -name '*.xml' | while read file;
 do
 	echo $file 
 	echo $file  >> $logfile_nocompress
-	timeout 60 $pathMB $file --bits=4 >> $logfile_nocompress 2>&1
+	timeout 60 $pathMB $file $options_for_all >> $logfile_nocompress #2>&1
 	if [ $? -eq 124 ]; then
 		echo "TIMEOUT" >> $logfile_nocompress
 	else
 		echo "SUCCESS" >> $logfile_nocompress
 	fi
 	echo $file >> $logfile_compress
-	timeout 60 $pathMB $file -c --bits=4 >> $logfile_compress 2>&1
+	timeout 60 $pathMB $file $options_for_all -c >> $logfile_compress #2>&1
 	if [ $? -eq 124 ]; then
 		echo "TIMEOUT" >> $logfile_compress
 	else
