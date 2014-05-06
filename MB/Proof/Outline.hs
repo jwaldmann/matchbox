6c8d17680c0314c518941ed5bda2830f9c7c3d72
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-05-07 00:00:29 +0200

    print proof outline at the end

--- MB/Proof/Outline.hs
+++ MB/Proof/Outline.hs
@@ -3,24 +3,37 @@
 module MB.Proof.Outline where
 
 import MB.Proof.Type
+import MB.Proof.Doc ()
 
+import TPDB.Data (strict, rules )
 import TPDB.Pretty 
-
+import Text.PrettyPrint.Leijen.Text ( align, indent)
 
 outline :: Proof v s -> Doc
-outline p = outreason $ reason p
+outline p = vcat
+    [ pretty (claim p) <+> "for" <+> outsys (input p)
+    , outreason $ reason p
+    ]
+
+outsys sys = 
+    let p = length $ filter strict $ rules sys
+        r = length $ filter (not . strict) $ rules sys
+    in  "TRS/DP with" <+> pretty p <+> "strict (pairs)" <+> pretty r <+> "relative (rules)"
 
 outreason r = case r of
     No_Strict_Rules -> "no strict pairs"
-    DP_Transform p -> vcat [ "DP transform", nest 4 $ outline p ]
+    DP_Transform p -> vcat [ "DP transform", indent 4 $ outline p ]
     Matrix_Interpretation_Arctic  i u p -> 
         vcat ["matrix interpretation, domain" <+> text  ( show $ domain i)
                       <+> "dimension" <+> pretty (dimension i)
-             , case u of Nothing -> empty ; Just _ -> "for usable rules"
-             , nest 4 $ outline p
+             , case u of 
+                     Nothing -> empty 
+                     Just us -> "for" <+> pretty (length us) <+> "usable rules" 
+             , indent 4 $ outline p
              ]
     SCCs cs ->  vcat ["EDG decomposed in SCCs" 
-                     , nest 4 $ vcat $ do Right c <- cs ; return $ outline c
+                     , indent 4 $ vcat $ do Right c <- cs ; return $ outline c
                      ]
-    Cpf2Cpf f p -> vcat [ "Cpf2Cpf", nest 4 $ outline p ]
+    Cpf2Cpf f p -> vcat [ "Cpf2Cpf", indent 4 $ outline p ]
+
 
