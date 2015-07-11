47cbe2abfc1a3d65ac943caa281e6b3115c8bea5
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-07-11 14:36:57 +0200

    new config

---
+++ release/starexec_run_plain.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+solver=boolector
+bits=4
+con=2
+
+export LD_LIBRARY_PATH=.
+
+./MB.exe  \
+    --$solver \
+    --natural \
+    --bits=$bits \
+    --con=$con \
+    --cores \
+    --latex \
+    +RTS -N -M32G -K1G -RTS \
+    $1  \
+    2>/dev/null
+
